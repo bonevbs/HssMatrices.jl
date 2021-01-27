e8036668d2bd440705e3f0b76fb3d7c51d7740db
Boris Bonev <bonevbs@gmail.com>
2021-01-27 16:22:08 +0100

    Conservative reworking of the current datastructure

--- src/hss_matrix.jl
+++ src/hss_matrix.jl
@@ -4,10 +4,10 @@
 ## data structure
 mutable struct HssMatrix{T<:Number} <: AbstractMatrix{T}
   # 2x2 recursive block structure for branchnodes
-  A11       ::HssMatrix{T}
-  A22       ::HssMatrix{T}
-  B12       ::Matrix{T}
-  B21       ::Matrix{T}
+  A11       ::Union{HssMatrix{T}, Nothing}
+  A22       ::Union{HssMatrix{T}, Nothing}
+  B12       ::Union{Matrix{T}, Nothing}
+  B21       ::Union{Matrix{T}, Nothing}
 
   # indicators whether we are at the root- or leafnode
   rootnode  ::Bool
@@ -20,18 +20,34 @@ mutable struct HssMatrix{T<:Number} <: AbstractMatrix{T}
   n2        ::Integer
 
   # translation operators if we are at a branchnode
-  R1        ::Matrix{T}
-  R2        ::Matrix{T}
-  W1        ::Matrix{T}
-  W2        ::Matrix{T}
+  R1        ::Union{Matrix{T}, Nothing}
+  R2        ::Union{Matrix{T}, Nothing}
+  W1        ::Union{Matrix{T}, Nothing}
+  W2        ::Union{Matrix{T}, Nothing}
 
   # at the leaf level we store the diagonal block and generators directly
-  U         ::Matrix{T}
-  V         ::Matrix{T}
-  D         ::Matrix{T}
+  D         ::Union{Matrix{T}, Nothing}
+  U         ::Union{Matrix{T}, Nothing}
+  V         ::Union{Matrix{T}, Nothing}
 
-  # inner constructor
-  HssMatrix{T}() where T = new{T}()
+  # inner constructors
+  HssMatrix{T}() where T = new{T}() # this is the dirty constructor that leaves stuff #undef
+  # rootnode constructor
+  function HssMatrix{T}(A11::HssMatrix{T}, B12::Matrix{T}, B21::Matrix{T}, A22::HssMatrix{T}) where T
+    m1, n1 = size(A11)
+    m2, n2 = size(A22)
+    if m1 != size(B12,1) throw(ArgumentError("A11 and B12 must have same number of rows")) end
+    if n1 != size(B21,2) throw(ArgumentError("A11 and B21 must have same number of columns")) end
+    if m2 != size(B21,1) throw(ArgumentError("A22 and B21 must have same number of rows")) end
+    if n2 != size(B12,2) throw(ArgumentError("A22 and B12 must have same number of columns")) end
+    new{T}(A11, B12, B21, A22, true, false, m1, n1, m2, n2, ntuple(x->nothing, 7)...)
+  end
+  # leafnode constructor
+  function HssMatrix{T}(D::Matrix{T}, U::Matrix{T}, V::Matrix{T}) where T
+    if size(D,1) != size(U,1) throw(ArgumentError("D and U must have same number of rows")) end
+    if size(D,2) != size(V,1) throw(ArgumentError("D and V must have same number of columns")) end
+    new{T}(ntuple(x->nothing, 4)..., true, false, m1, n1, m2, n2, ntuple(x->nothing, 4)..., D, U, V)
+  end
 end
 
 # make element type extraction work
@@ -42,27 +58,27 @@ eltype(::Type{HssMatrix{T}}) where {T} = T
 function copy(hssA::HssMatrix{T}) where {T}
   hssB = HssMatrix{T}()
 
-  if isdefined(hssA, :A11); hssB.A11 = copy(hssA.A11); end
-  if isdefined(hssA, :A22); hssB.A22 = copy(hssA.A22); end
-  if isdefined(hssA, :B12); hssB.B12 = copy(hssA.B12); end
-  if isdefined(hssA, :B21); hssB.B21 = copy(hssA.B21); end
+  hssB.A11 = copy(hssA.A11)
+  hssB.A22 = copy(hssA.A22)
+  hssB.B12 = copy(hssA.B12)
+  hssB.B21 = copy(hssA.B21)
 
-  if isdefined(hssA, :rootnode); hssB.rootnode = hssA.rootnode; end
-  if isdefined(hssA, :leafnode); hssB.leafnode = hssA.leafnode; end
+  hssB.rootnode = hssA.rootnode
+  hssB.leafnode = hssA.leafnode
 
-  if isdefined(hssA, :m1); hssB.m1 = hssA.m1; end
-  if isdefined(hssA, :n1); hssB.n1 = hssA.n1; end
-  if isdefined(hssA, :m2); hssB.m2 = hssA.m2; end
-  if isdefined(hssA, :n2); hssB.n2 = hssA.n2; end
+  hssB.m1 = hssA.m1
+  hssB.n1 = hssA.n1
+  hssB.m2 = hssA.m2
+  hssB.n2 = hssA.n2
 
-  if isdefined(hssA, :R1); hssB.R1 = copy(hssA.R1); end
-  if isdefined(hssA, :R2); hssB.R2 = copy(hssA.R2); end
-  if isdefined(hssA, :W1); hssB.W1 = copy(hssA.W1); end
-  if isdefined(hssA, :W2); hssB.W2 = copy(hssA.W2); end
+  hssB.R1 = copy(hssA.R1)
+  hssB.R2 = copy(hssA.R2)
+  hssB.W1 = copy(hssA.W1)
+  hssB.W2 = copy(hssA.W2)
 
-  if isdefined(hssA, :U); hssB.U = copy(hssA.U); end
-  if isdefined(hssA, :V); hssB.V = copy(hssA.V); end
-  if isdefined(hssA, :D); hssB.D = copy(hssA.D); end
+  hssB.U = copy(hssA.U)
+  hssB.V = copy(hssA.V)
+  hssB.D = copy(hssA.D)
 
   return(hssB)
 end
@@ -94,7 +110,7 @@ end
 # construct full matrix from HSS
 function Base.Matrix(hssA::HssMatrix{T}) where {T}
   n = size(hssA,2)
-  return hssA * Matrix{T}(I, n, n)
+  return hssA * Union{Matrix{T}, Nothing}(I, n, n)
 end
 
 # alternatively we can form the full matrix in a more straight-forward fashion
